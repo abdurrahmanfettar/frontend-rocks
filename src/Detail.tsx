bafbb945691b137153035c7f3e40ddb963343717
abdurrahmanfettar <abdurrahmanfettar@gmail.com>
2026-01-15 13:22:13 +0000

    prima versione

--- src/Detail.tsx
+++ src/Detail.tsx
@@ -1,3 +1,14 @@
 export const Detail = () => {
-  return null;
+  const cardstyle = "bg-green-500 w-30 h-30 text-1xl text-center flex items-center justify-center";
+  return <div className="text-5xl space-x-2">
+  <div className={cardstyle}>1</div>;
+  <div className={cardstyle}>2</div>;
+  <div className={cardstyle}>3</div>;
+  </div>
+  
+  
+
+  
+ 
+
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,48 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router";
 
+
 export const Root = () => {
-  const [count, setCount] = useState(0);
-  const [title, setTitle] = useState("Titolo iniziale");
+  return <div className="space-x-2 space-y-2 flex flex-wrap text-l">
+      <Card title="pikatchu - 0" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" />
+      <Card title="02" image="" />
+    </div>};
 
-  useEffect(() => {
-    if (count === 4) {
-      setTitle("Il titolo ha superato il 4");
-    }
-  }, [count]);
 
-  return (
-    <div className="h-dvh flex flex-col items-center justify-center">
-      <div className="bg-white p-8 rounded-md shadow-lg">
-        <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-          {title}
-        </h1>
-
-        <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
-
-        <div className="flex flex-col items-center space-y-4">
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setCount((count) => count + 1)}
-          >
-            Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
-          </button>
-
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setTitle("Charizard")}
-          >
-            Cambia titolo
-          </button>
-
-          <Link
-            to="/frontend-rocks/dettaglio/1"
-            className="text-blue-500 underline"
-          >
-            Link alla pagina di dettaglio
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-};
