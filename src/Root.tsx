474a92ade5d6b416f5f02164a2595524a45d1c54
abdurrahmanfettar <abdurrahmanfettar@gmail.com>
2026-01-15 13:27:22 +0000

    first

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,41 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router";
+import React from "react";
 
+type CardProps = {
+  title: string;
+  image: string;
+};
 
 export const Root = () => {
-  return <div className="space-x-2 space-y-2 flex flex-wrap text-l">
-      <Card title="pikatchu - 0" image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" />
-      <Card title="02" image="" />
-    </div>};
-    const Card = ({title, image}: {title: string, image: string}) => {
-      return <div className="bg-white w-40 h-40"
+  return (
+    <div className="flex flex-wrap gap-2 text-lg">
+      <Card
+        title="Pikachu - 0"
+        image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+      />
+      <Card
+        title="02"
+        image=""
+      />
+    </div>
+  );
+};
+
+const Card = ({ title, image }: CardProps) => {
+  const placeholder =
+    "https://via.placeholder.com/160x160?text=No+Image";
+
+  return (
+    <div className="bg-white w-40 h-40 rounded-md shadow-md overflow-hidden">
+      <img
+        src={image || placeholder}
+        alt={title}
+        className="w-full h-28 object-cover"
+      />
+      <div className="p-2 text-center font-semibold">
+        {title}
+      </div>
+    </div>
+  );
+};
 
 
